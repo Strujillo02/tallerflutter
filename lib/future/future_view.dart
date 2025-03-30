7e3de68c5f2291230ab76337a231804d52ba115e
sebastian trujillo <sebastian.trujillo02@uceva.edu.co>
2025-03-30 17:37:21 -0500

    se añadio los comentarios

--- lib/future/future_view.dart
+++ lib/future/future_view.dart
@@ -1,93 +1,102 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:tallergorouter/views/base_view.dart';
+import 'dart:async'; // Importa la librería para trabajar con Future y asincronía.
+import 'package:flutter/material.dart'; // Importa los widgets y herramientas de Flutter.
+import 'package:tallergorouter/views/base_view.dart'; // Importa una vista base personalizada.
 
 class FutureView extends StatefulWidget {
-  const FutureView({super.key});
+  // Declara un StatefulWidget llamado FutureView.
+  const FutureView({super.key}); // Constructor de la clase FutureView.
 
   @override
-  State<FutureView> createState() => _FutureViewState();
+  State<FutureView> createState() => _FutureViewState(); // Crea el estado asociado al widget.
 }
 
 class _FutureViewState extends State<FutureView> {
-  List<String> _nombres = []; //forma para declarar una lista.
+  // Clase que define el estado del widget FutureView.
+  List<String> _nombres =
+      []; // Declara una lista vacía para almacenar los nombres.
 
   @override
-  // !inicializa el estado
-  // *llama a la funcion obtenerDatos() para cargar los datos al iniciar
   void initState() {
-    super.initState();
-    obtenerDatos(); // carga al iniciar
+    // Método que se ejecuta al inicializar el estado del widget.
+    super.initState(); // Llama al método initState de la clase padre.
+    obtenerDatos(); // Llama a la función obtenerDatos para cargar los datos al iniciar.
   }
 
-  // !Funcion que simula una carga de datos
-  //*espera 5 segundos antes de cargar los datos, esto simula una carga de datos.
   Future<List<String>> cargarNombres() async {
-    //future.delayed() simula una carga de datos
-    await Future.delayed(const Duration(seconds: 5));
+    // Función que simula la carga de datos.
+    await Future.delayed(
+      const Duration(seconds: 5),
+    ); // Espera 5 segundos para simular una carga.
     return [
-      'Juan',
-      'Pedro',
-      'Luis',
-      'Ana',
-      'Maria',
-      'Jose',
-      'Carlos',
-      'Sofia',
-      'Laura',
-      'Fernando',
-      'Ricardo',
-      'Diana',
-      'Elena',
-      'Miguel',
-      'Rosa',
-      'Luz',
-      'Carmen',
-      'Pablo',
-      'Jorge',
-      'Roberto',
+      // Retorna una lista de nombres después de la espera.
+      'Juan', 'Pedro', 'Luis', 'Ana', 'Maria', 'Jose', 'Carlos', 'Sofia',
+      'Laura', 'Fernando', 'Ricardo', 'Diana', 'Elena', 'Miguel', 'Rosa',
+      'Luz', 'Carmen', 'Pablo', 'Jorge', 'Roberto',
     ];
   }
 
-  // !Funcion que obtiene los datos
-  // *carga los datos y los asigna a la lista _nombres
   Future<void> obtenerDatos() async {
-    final datos = await cargarNombres();
+    // Función que obtiene los datos y los asigna a la lista.
+    final datos =
+        await cargarNombres(); // Llama a cargarNombres y espera los datos.
 
-    //!mounted es una propiedad de State que indica si el widget está montado en el árbol de widgets
-    //mounted es true si el widget está montado en el árbol de widgets
-    //mounted es false si el widget no está montado en el árbol de widgets
-    if (!mounted) return;
+    if (!mounted)
+      return; // Verifica si el widget sigue montado antes de actualizar el estado.
     setState(() {
-      _nombres = datos;
+      // Actualiza el estado del widget.
+      _nombres = datos; // Asigna los datos obtenidos a la lista _nombres.
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // Método que construye la interfaz del widget.
     return BaseView(
-      title: 'Lista de estudiantes',
-      body:
-          //*si la lista esta vacia muestra un CircularProgressIndicator
-          _nombres.isEmpty
-              ? const Center(child: CircularProgressIndicator())
+      // Retorna un widget BaseView personalizado.
+      title: 'Lista de estudiantes', // Título de la vista.
+      body: // Cuerpo de la vista.
+          _nombres
+                  .isEmpty // Verifica si la lista _nombres está vacía.
+              ? const Center(
+                child: CircularProgressIndicator(),
+              ) // Muestra un indicador de carga si está vacía.
               : Padding(
-                padding: const EdgeInsets.all(10.0),
+                // Si no está vacía, muestra el contenido con un padding.
+                padding: const EdgeInsets.all(
+                  10.0,
+                ), // Define un padding de 10 píxeles.
                 child: GridView.builder(
-                  itemCount: _nombres.length,
+                  // Crea un GridView dinámico.
+                  itemCount:
+                      _nombres.length, // Número de elementos en la lista.
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1, // columnas
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    childAspectRatio: 2,
+                    // Configura el diseño del grid.
+                    crossAxisCount: 1, // Número de columnas en el grid.
+                    mainAxisSpacing:
+                        10, // Espaciado vertical entre los elementos.
+                    crossAxisSpacing:
+                        10, // Espaciado horizontal entre los elementos.
+                    childAspectRatio:
+                        2, // Relación de aspecto de los elementos.
                   ),
                   itemBuilder: (context, index) {
+                    // Construye cada elemento del grid.
                     return Card(
-                      color: const Color.fromARGB(255, 87, 194, 180),
+                      // Retorna un widget Card para cada elemento.
+                      color: const Color.fromARGB(
+                        255,
+                        87,
+                        194,
+                        180,
+                      ), // Color de fondo del Card.
                       child: Center(
+                        // Centra el contenido dentro del Card.
                         child: Text(
-                          _nombres[index],
-                          style: const TextStyle(fontSize: 18),
+                          // Muestra el nombre en el centro del Card.
+                          _nombres[index], // Obtiene el nombre correspondiente al índice.
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ), // Estilo del texto (tamaño de fuente 18).
                         ),
                       ),
                     );
--- lib/views/timer/timer_view.dart
+++ lib/views/timer/timer_view.dart
@@ -1,139 +1,165 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:tallergorouter/views/base_view.dart';
+import 'dart:async'; // Importa la librería para trabajar con temporizadores y asincronía.
+import 'package:flutter/material.dart'; // Importa los widgets y herramientas de Flutter.
+import 'package:tallergorouter/views/base_view.dart'; // Importa una vista base personalizada.
 
 class TimerView extends StatefulWidget {
-  const TimerView({super.key});
+  // Declara un StatefulWidget llamado TimerView.
+  const TimerView({super.key}); // Constructor de la clase TimerView.
 
   @override
-  State<TimerView> createState() => _TimerViewState();
+  State<TimerView> createState() => _TimerViewState(); // Crea el estado asociado al widget.
 }
 
 class _TimerViewState extends State<TimerView> {
-  //late indica que la variable será inicializada después
-  //esto se usa con el fin de que la variable no sea nula
-  late Timer _timer;
-  int _contador = 0;
-  int _indiceSeleccionado = 0; //indice seleccionado en el BottomNavigationBar
+  // Clase que define el estado del widget TimerView.
+  late Timer _timer; // Declara un temporizador que será inicializado más tarde.
+  int _contador = 0; // Variable para almacenar el valor del contador.
+  int _indiceSeleccionado = 0; // Índice seleccionado en el BottomNavigationBar.
 
   @override
-  //!initState() se llama una vez que el widget ha sido creado
-  //!y se puede utilizar para inicializar datos que solo deben hacerse una vez.
   void initState() {
-    super.initState();
+    // Método que se ejecuta al inicializar el estado del widget.
+    super.initState(); // Llama al método initState de la clase padre.
   }
 
-  //! Método para iniciar el temporizador
   void _iniciarTemporizador() {
-    //timer.periodic() ejecuta una función cada cierto tiempo
-    //en este caso cada segundo
+    // Método para iniciar el temporizador.
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      // Ejecuta una función cada segundo.
       setState(() {
-        _contador++;
+        // Actualiza el estado del widget.
+        _contador++; // Incrementa el contador en 1.
       });
     });
   }
 
   void _reiniciarTemporizador() {
-    //timer.periodic() ejecuta una función cada cierto tiempo
-    //en este caso cada segundo
-    _contador = 0;
+    // Método para reiniciar el temporizador.
+    _contador = 0; // Reinicia el contador a 0.
   }
 
-  //metodo para pausar el temporizador
   void _pausarTemporizador() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _timer.cancel();
-      });
-    });
+    // Método para pausar el temporizador.
+    _timer.cancel(); // Cancela el temporizador.
   }
 
   @override
-  //! dispose() se llama cuando el widget se elimina del árbol de widgets
-  //! y se puede utilizar para liberar recursos o cancelar temporizadores.
   void dispose() {
-    //! _timer.cancel() cancela el temporizador, requerido siempre que se use Timer
-    //! evita fugas de memoria
-    _timer.cancel();
-    super.dispose();
+    // Método que se ejecuta cuando el widget se elimina del árbol de widgets.
+    _timer.cancel(); // Cancela el temporizador para evitar fugas de memoria.
+    super.dispose(); // Llama al método dispose de la clase padre.
   }
 
-  //! Método para cambiar de sección
   void _itemSeleccionado(int index) {
+    // Método para cambiar de sección en el BottomNavigationBar.
     setState(() {
-      _indiceSeleccionado = index;
+      // Actualiza el estado del widget.
+      _indiceSeleccionado = index; // Cambia el índice seleccionado.
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    //*Lista de Widgets para las páginas
+    // Método que construye la interfaz del widget.
     List<Widget> paginas = [
+      // Lista de widgets para las páginas.
       Center(
+        // Página que muestra el contador.
         child: Text(
-          'Segundos: $_contador',
-          style: const TextStyle(fontSize: 28),
+          // Muestra el valor del contador.
+          'Segundos: $_contador', // Texto con el valor del contador.
+          style: const TextStyle(
+            fontSize: 28,
+          ), // Estilo del texto (tamaño de fuente 28).
         ),
       ),
-
       const Padding(
-        padding: EdgeInsets.all(16.0),
+        // Página que muestra información sobre el temporizador.
+        padding: EdgeInsets.all(
+          16.0,
+        ), // Padding de 16 píxeles alrededor del texto.
         child: Text(
+          // Texto explicativo.
           'Este ejemplo usa Timer.periodic para aumentar el contador '
-          'automáticamente cada segundo. ',
-          style: TextStyle(fontSize: 18),
+          'automáticamente cada segundo. ', // Explicación del funcionamiento del temporizador.
+          style: TextStyle(
+            fontSize: 18,
+          ), // Estilo del texto (tamaño de fuente 18).
         ),
       ),
     ];
 
     return BaseView(
-      title: 'Timer - TabBar ',
+      // Retorna un widget BaseView personalizado.
+      title: 'Timer - TabBar ', // Título de la vista.
       body: Column(
+        // Cuerpo de la vista.
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // Fila con los botones de control del temporizador.
+            mainAxisAlignment:
+                MainAxisAlignment
+                    .spaceAround, // Distribuye los botones de manera uniforme.
             children: <Widget>[
               SizedBox(
-                width: 75,
-                height: 75,
+                // Botón para pausar el temporizador.
+                width: 75, // Ancho del botón.
+                height: 75, // Alto del botón.
                 child: ElevatedButton(
-                  onPressed: () => _timer.cancel(),
-                  child: Icon(Icons.pause),
+                  // Botón elevado.
+                  onPressed:
+                      () =>
+                          _pausarTemporizador(), // Llama al método para pausar el temporizador.
+                  child: Icon(Icons.pause), // Icono de pausa.
                 ),
               ),
               SizedBox(
-                width: 75,
-                height: 75,
+                // Botón para reiniciar el temporizador.
+                width: 75, // Ancho del botón.
+                height: 75, // Alto del botón.
                 child: ElevatedButton(
-                  onPressed: () => _reiniciarTemporizador(),
-                  child: Icon(Icons.restore),
+                  // Botón elevado.
+                  onPressed:
+                      () =>
+                          _reiniciarTemporizador(), // Llama al método para reiniciar el temporizador.
+                  child: Icon(Icons.restore), // Icono de reinicio.
                 ),
               ),
               SizedBox(
-                width: 75,
-                height: 75,
+                // Botón para iniciar el temporizador.
+                width: 75, // Ancho del botón.
+                height: 75, // Alto del botón.
                 child: ElevatedButton(
-                  onPressed: () => _iniciarTemporizador(),
-                  child: Icon(Icons.play_arrow),
+                  // Botón elevado.
+                  onPressed:
+                      () =>
+                          _iniciarTemporizador(), // Llama al método para iniciar el temporizador.
+                  child: Icon(Icons.play_arrow), // Icono de reproducción.
                 ),
               ),
             ],
           ),
-          Expanded(child: paginas[_indiceSeleccionado]),
+          Expanded(
+            child: paginas[_indiceSeleccionado],
+          ), // Muestra la página seleccionada.
           BottomNavigationBar(
-            currentIndex: _indiceSeleccionado,
-            onTap: _itemSeleccionado,
-            selectedItemColor: Colors.blueAccent,
-            unselectedItemColor: Colors.grey,
+            // Barra de navegación inferior.
+            currentIndex:
+                _indiceSeleccionado, // Índice de la página seleccionada.
+            onTap: _itemSeleccionado, // Cambia la página al tocar un ítem.
+            selectedItemColor:
+                Colors.blueAccent, // Color del ítem seleccionado.
+            unselectedItemColor:
+                Colors.grey, // Color de los ítems no seleccionados.
             items: const [
+              // Ítems del BottomNavigationBar.
               BottomNavigationBarItem(
-                icon: Icon(Icons.timer),
-                label: 'Contador',
+                icon: Icon(Icons.timer), // Icono del primer ítem.
+                label: 'Contador', // Etiqueta del primer ítem.
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.restore),
-                label: 'Reiniciar',
+                icon: Icon(Icons.restore), // Icono del segundo ítem.
+                label: 'Reiniciar', // Etiqueta del segundo ítem.
               ),
             ],
           ),
